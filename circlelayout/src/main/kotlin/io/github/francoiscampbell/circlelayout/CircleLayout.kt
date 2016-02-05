b2bba119c4bee094119085f7bfa56effc8786cbe
Francois Campbell <campbell.francois@gmail.com>
2016-02-04 23:55:56 -0500

    Made layout mutable to allow it to be modified at runtime. Cache center view to avoid findViewById at every layout pass

--- circlelayout/src/main/kotlin/io/github/francoiscampbell/circlelayout/CircleLayout.kt
+++ circlelayout/src/main/kotlin/io/github/francoiscampbell/circlelayout/CircleLayout.kt
@@ -14,27 +14,55 @@ import java.util.*
  */
 class CircleLayout(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : ViewGroup(context, attrs, defStyleAttr) {
 
-    private val centerViewId: Int
-    private val angle: Float
-    private val angleOffset: Float
-    private val fixedRadius: Int
-    private val radiusOverride: Int
-    private val direction: Int
-
-    private val childrenToLayout = LinkedList<View>()
+    var angle: Float
+    var angleOffset: Float
+    var fixedRadius: Int
+    var radiusPreset = FITS_LARGEST_CHILD
+        set(newRadiusPreset: Int) = when {
+            newRadiusPreset != FITS_LARGEST_CHILD && newRadiusPreset != FITS_SMALLEST_CHILD -> {
+                throw IllegalArgumentException("radiusPreset must be either FITS_LARGEST_CHILD or FITS_SMALLEST_CHILD")
+            }
+            else -> field = newRadiusPreset
+        }
+    var direction = COUNTER_CLOCKWISE
+        set(newDirection: Int) = when {
+            newDirection > 0 -> field = 1
+            newDirection < 0 -> field = -1
+            else -> throw IllegalArgumentException("direction must be either positive or negative")
+        }
 
     val layoutHasCenterView: Boolean
         get() = centerViewId != View.NO_ID
 
+    private var centerViewId: Int
+    var centerView: View? = null
+        set(newCenterView: View?) = when {
+            newCenterView != null && indexOfChild(newCenterView) == -1 -> {
+                throw IllegalArgumentException("View with ID ${newCenterView.id} is not a child of this layout")
+            }
+            else -> {
+                field = newCenterView
+                centerViewId = newCenterView?.id ?: NO_ID
+            }
+        }
+
+    private val childrenToLayout = LinkedList<View>()
+
     init {
         val attributes = context.obtainStyledAttributes(attrs, R.styleable.cl_CircleLayout, defStyleAttr, 0)
-        centerViewId = attributes.getResourceId(R.styleable.cl_CircleLayout_cl_centerView, View.NO_ID)
+        centerViewId = attributes.getResourceId(R.styleable.cl_CircleLayout_cl_centerView, NO_ID)
         angle = Math.toRadians(attributes.getFloat(R.styleable.cl_CircleLayout_cl_angle, 0f).toDouble()).toFloat()
         angleOffset = Math.toRadians(attributes.getFloat(R.styleable.cl_CircleLayout_cl_angleOffset, 0f).toDouble()).toFloat()
         fixedRadius = attributes.getDimensionPixelSize(R.styleable.cl_CircleLayout_cl_radius, 0)
-        radiusOverride = attributes.getInt(R.styleable.cl_CircleLayout_cl_radiusPreset, FITS_LARGEST_CHILD)
+        radiusPreset = attributes.getInt(R.styleable.cl_CircleLayout_cl_radiusPreset, FITS_LARGEST_CHILD)
         direction = attributes.getInt(R.styleable.cl_CircleLayout_cl_direction, COUNTER_CLOCKWISE)
         attributes.recycle()
+    }
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+
+        centerView = findViewById(centerViewId)
     }
 
     public override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -54,9 +82,7 @@ class CircleLayout(context: Context, attrs: AttributeSet? = null, defStyleAttr: 
         val centerY = paddingRight + displayAreaHeight / 2
         val outerRadius = Math.min(displayAreaWidth, displayAreaHeight) / 2
 
-        val centerView = findViewById(centerViewId)
         centerView?.layoutFromCenter(centerX, centerY)
-
 
         var minChildRadius = outerRadius
         var maxChildRadius = 0
@@ -66,14 +92,8 @@ class CircleLayout(context: Context, attrs: AttributeSet? = null, defStyleAttr: 
                 return@forEachChild
             }
             childrenToLayout.add(this)
-            when {
-                radius > maxChildRadius -> {
-                    maxChildRadius = radius
-                }
-                radius < minChildRadius -> {
-                    minChildRadius = radius
-                }
-            }
+            maxChildRadius = Math.max(maxChildRadius, radius)
+            minChildRadius = Math.min(minChildRadius, radius)
         }
         //choose angle increment
         val angleIncrement = if (angle != 0f) angle else getEqualAngle(childrenToLayout.size)
@@ -84,11 +104,17 @@ class CircleLayout(context: Context, attrs: AttributeSet? = null, defStyleAttr: 
         layoutChildrenAtAngle(centerX, centerY, angleIncrement, angleOffset, layoutRadius, childrenToLayout)
     }
 
+    /**
+     * @param outerRadius The outer radius of this layout's display area
+     * @param maxChildRadius The radius of the largest child
+     * @param minChildRadius The radius of the smallest child
+     * @return The radius of the layout path along which the children will be placed
+     */
     private fun getLayoutRadius(outerRadius: Int, maxChildRadius: Int, minChildRadius: Int): Int {
-        when (radiusOverride) {
-            FITS_LARGEST_CHILD -> return outerRadius - maxChildRadius
-            FITS_SMALLEST_CHILD -> return outerRadius - minChildRadius
-            else -> return outerRadius - maxChildRadius
+        return when (radiusPreset) {
+            FITS_LARGEST_CHILD -> outerRadius - maxChildRadius
+            FITS_SMALLEST_CHILD -> outerRadius - minChildRadius
+            else -> outerRadius - maxChildRadius
         }
     }
 
@@ -100,12 +126,13 @@ class CircleLayout(context: Context, attrs: AttributeSet? = null, defStyleAttr: 
     private fun getEqualAngle(numSlices: Int): Float = 2 * Math.PI.toFloat() / if (numSlices != 0) numSlices else 1
 
     /**
-     * Lays out the visible child views along the circle
+     * Lays out the child views along a circle
+     * @param cx                    The X coordinate of the center of the circle
      * @param cy                    The Y coordinate of the center of the circle
      * @param angleIncrement        The angle increment between two adjacent children
      * @param angleOffset           The starting offset angle from the horizontal axis
      * @param radius                The radius of the circle along which the centers of the children will be placed
-     * @param childrenToLayout      The views to layout. Any null views are ignored
+     * @param childrenToLayout      The views to layout
      */
     private fun layoutChildrenAtAngle(cx: Int, cy: Int, angleIncrement: Float, angleOffset: Float, radius: Int, childrenToLayout: List<View>) {
         var currentAngle = angleOffset
